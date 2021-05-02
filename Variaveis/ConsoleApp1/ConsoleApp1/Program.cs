4c490527456827b09852aadd2362f26a8e87e150
João Godinho <joaoeustaquiosg@gmail.com>
2021-05-02 18:28:05 -0300

    Inicial

---
+++ Variaveis/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			// Int -- -156 ou 32334 (numeros inteiros)
+			// Float -- -15.6 ou 232.132 (numeros decimais)
+			// Bool(Boolean) -- true ou false ()
+			// String -- "asdfasdfasd asdfasdf" (tipo texto)
+			// Char -- 'a' (caracter unico)
+
+			int segundaGuerraMundial = 1942;
+			string cor_Favorita = "Vermelho";
+			float velocidadeF1 = 294.48f;
+			bool segundaGuerraMundial = true;
+
+
+			Console.WriteLine("Hello World");
+
+			Console.WriteLine("Meu nome é João Godinho");
+
+			Console.ReadLine();
+		}
+	}
+}
